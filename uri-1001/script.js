d9095db41d9919ce0488e2d9fb77e9422476bb1e
cleysonxp <cleyson.ribeiro.777@gmail.com>
2020-03-04 17:09:46 -0300

    Resolção dos exercicios do site URI em JavaScript

---
+++ uri-1001/script.js
@@ -0,0 +1,7 @@
+let a = parseInt(prompt());
+let b = parseInt(prompt());
+
+let x = a + b;
+
+// console.log(x);
+alert(x);
---
+++ uri-1002/script.js
@@ -0,0 +1,7 @@
+let raio = parseFloat(prompt());
+
+let r = 3.14159;
+
+let area = r * (raio * raio);
+
+alert("A= "+area);
